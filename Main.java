b3be300d53fdbcd9bddaa2e397ed9ef0352c9415
zesha5816 <zesha5816@wrdsb.ca>
2020-09-14 14:54:15 +0000

    finished the examplar

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *program that will ask the user for two numbers and then calculate the quotient of that number and the remainder
+ * @author Ahmad Zeshan 
  */
 public class Main {
 
@@ -9,7 +10,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Please enter in two integers, on seperate lines, to divide.");
+		int dividend = input.nextInt();
+		int divisor = input.nextInt();
     // TODO code application logic here
-    
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+		System.out.println(dividend + "/" + divisor + " is " + quotient + " with a remainder of " + remainder);
+        
+				
   }
 }
